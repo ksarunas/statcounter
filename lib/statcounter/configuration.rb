9a722e5bf4c97f540cb86d4c14b92a98b21c9136
Šarūnas Kūjalis <sarjalis@gmail.com>
2016-06-23 09:40:25 +0300

    Add timezone to config

--- lib/statcounter/configuration.rb
+++ lib/statcounter/configuration.rb
@@ -1,9 +1,13 @@
 module Statcounter
   class Configuration
-    attr_accessor :username, :secret, :timeout
+    attr_accessor :username, :secret, :timeout, :timezone
 
     def timeout
       @timeout ||= 60
+    end
+
+    def timezone
+      @timezone ||= 'America/New_York'
     end
   end
 end
