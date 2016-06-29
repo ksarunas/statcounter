462f8523c10d3a4b10d521c27a57428d83391fa5
Šarūnas Kūjalis <sarjalis@gmail.com>
2016-06-29 16:46:16 +0300

    Add summary stats daily endpoint wrapper

---
+++ lib/statcounter/summary_stats.rb
@@ -0,0 +1,25 @@
+module Statcounter
+  class SummaryStats
+    def self.daily(project_ids:, date_from:, date_to:, credentials: nil)
+      params = {
+        s: :summary,
+        g: :daily,
+        pi: project_ids,
+        sd: date_from.day,
+        sm: date_from.month,
+        sy: date_from.year,
+        ed: date_to.day,
+        em: date_to.month,
+        ey: date_to.year,
+      }
+      response = Statcounter.client.get('stats', params: params, credentials: credentials)
+      if response[:project]
+        response[:project].each_with_object({}) do |project, result|
+          result[project[:id].to_i] = project[:sc_data]
+        end
+      else
+        response[:sc_data].size > 1 ? response[:sc_data] : response[:sc_data][0]
+      end
+    end
+  end
+end
---
+++ spec/summary_stats_spec.rb
@@ -0,0 +1,51 @@
+RSpec.describe Statcounter::SummaryStats do
+  before { Timecop.freeze(Time.at(1466614800)) }
+
+  describe '.daily' do
+    subject do
+      described_class.daily(
+        project_ids: project_ids,
+        date_from: date_from,
+        date_to: date_to,
+        credentials: default_credentials
+      )
+    end
+
+    let(:project_ids) { 1 }
+    let(:date_from) { Date.new(2016, 6, 28) }
+    let(:date_to) { Date.new(2016, 6, 28) }
+    let(:request_uri) { 'http://api.statcounter.com/stats?s=summary&g=daily&pi=1&sd=28&sm=6&sy=2016&ed=28&em=6&ey=2016&vn=3&t=1466614800&u=john_brown&f=json&sha1=9c16ca0e1034bc8d878396b0c34a5232e971ce35' }
+    let(:response_file) { 'spec/assets/summary_stats_daily.json' }
+
+    before { stub_request(:get, request_uri).to_return(body: File.read(response_file)) }
+
+    it 'returns summery stats' do
+      expect(subject).to be_instance_of Hash
+      expect(subject).to include(
+        :date,
+        :page_views,
+        :unique_visits,
+        :returning_visits,
+        :first_time_visits,
+      )
+    end
+
+    context 'when multiple ids passed' do
+      let(:project_ids) { [1, 2] }
+      let(:request_uri) { 'http://api.statcounter.com/stats?s=summary&g=daily&pi=1&pi=2&sd=28&sm=6&sy=2016&ed=28&em=6&ey=2016&vn=3&t=1466614800&u=john_brown&f=json&sha1=ab0a3d2be2b3a7270ee1c2c0655e1cd7c3de818e' }
+      let(:response_file) { 'spec/assets/summary_stats_daily_multiple.json' }
+
+      it 'returns multiple project summery stats' do
+        expect(subject).to be_instance_of Hash
+        expect(subject.keys.size).to eq 2
+        expect(subject[2][0]).to include(
+          :date,
+          :page_views,
+          :unique_visits,
+          :returning_visits,
+          :first_time_visits,
+        )
+      end
+    end
+  end
+end
